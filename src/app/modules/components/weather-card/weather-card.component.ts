a25c0ff6601cc81293b4b5f2e34a0136b6738e1f
freiremat <urbsfreire@gmail.com>
2025-12-01 12:00:09 -0300

    tasks: - implement layout on weather-card html

--- src/app/modules/components/weather-card/weather-card.component.ts
+++ src/app/modules/components/weather-card/weather-card.component.ts
@@ -1,19 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
 import { weatherDatas } from 'src/app/models/interfaces/WeatherDatas';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
+  styles: [],
+
 })
-export class WeatherCardComponent implements OnInit {
+export class WeatherCardComponent {
+  //dados da previsao do tempo vindos do comp pai
   @Input() weatherDatasInput !: weatherDatas;
 
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+
   constructor() { }
-
-  ngOnInit() {
-    console.log('dados check', this.weatherDatasInput);
-
-  }
 
 }
 
